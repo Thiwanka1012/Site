269be68b727c226ed3ab0003c90c3eb732330e88
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-09 12:42:55 +0530

    Add initial HTML structure and CSS file

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Website Title</title>
+</head>
+<body>
+
+	<header>
+		<h1>Website Name</h1>\
+		<link rel="stylesheet" href="css/main.css">
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="home.php">Home</a></li>
+			<li><a href="about-us.php">Avout Us</a></li>
+			<li><a href="products.php"></a>Products</li>
+			<li><a href="contact-us.php"></a>Contact Us</li>
+			
+		</ul>
+	</nav>
+
+	<main>
+		<article>
+			<h1>Home</h1>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia dolorum magni enim optio repellat, ducimus tempora, debitis quos at accusantium id assumenda incidunt perferendis voluptatem porro ea voluptatum dolores fuga.</p>
+			<p>Optio cumque repellendus commodi necessitatibus in cum laboriosam veniam illum ut omnis placeat est non maxime, animi ex quas eum quis ipsa. Iure dolore rerum adipisci obcaecati quod. Vel, ratione?</p>
+			<p>Nobis, quod nam. Beatae dolores voluptas minima totam odio provident perspiciatis blanditiis, quibusdam dignissimos ut. Earum ipsam adipisci sapiente deserunt ipsum libero! Similique animi officia dolor non aliquam modi sequi.</p>
+		</article>
+	</main>
+
+	<footer>
+		<p>Company Name | Address | Tel : 5555555</p>
+	</footer>
+
+</body>
+</html>
