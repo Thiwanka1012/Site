4564e476909362135a12a3b407935aaec225b645
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-10 10:13:18 +0530

    Remove home.php and restructure index.php to include header, navigation, and footer directly

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php include('includes/header.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Website Title</title>
+</head>
+<body>
+
+	<header>
+		<h1>Website Name</h1>
+		<link rel="stylesheet" href="css/main.css">
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="home.php">Home</a></li>
+			<li><a href="about-us.php">About Us</a></li>
+			<li><a href="products.php">Products</a></li>
+			<li><a href="contact-us.php">Contact Us</a></li>
+			
+		</ul>
+	</nav>
 
 	<main>
 		<article>
@@ -9,4 +31,9 @@
 		</article>
 	</main>
 
-<?php include('includes/footer.php'); ?>
+	<footer>
+		<p>Company Name | Address | Tel : 5555555</p>
+	</footer>
+
+</body>
+</html>
