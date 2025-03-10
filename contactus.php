d12ae4f73916f6659d0020dfbc8b58477572741b
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-10 09:59:10 +0530

    Add Contact Us page with header and footer includes

---
+++ contactus.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php'); ?>
+<main>
+    <article>
+        <h1>Contact Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quaerat aperiam eveniet cum incidunt perspiciatis praesentium esse aliquam nostrum error assumenda nesciunt facere officiis eligendi, nam aliquid harum repellendus! Eligendi!</p>
+    </article>
+</main>
+<?php include('includes/footer/php'); ?>
