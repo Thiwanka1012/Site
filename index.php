5ed73d8dfdb3179ad8242c8963b4036beaaf6015
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-10 09:48:51 +0530

    Refactor index.php to include header and footer from separate files

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Website Title</title>
-</head>
-<body>
-
-	<header>
-		<h1>Website Name</h1>
-		<link rel="stylesheet" href="css/main.css">
-	</header>
-
-	<nav>
-		<ul>
-			<li><a href="home.php">Home</a></li>
-			<li><a href="about-us.php">About Us</a></li>
-			<li><a href="products.php">Products</a></li>
-			<li><a href="contact-us.php">Contact Us</a></li>
-			
-		</ul>
-	</nav>
+<?php include('includes/header.php'); ?>
 
 	<main>
 		<article>
@@ -31,9 +9,4 @@
 		</article>
 	</main>
 
-	<footer>
-		<p>Company Name | Address | Tel : 5555555</p>
-	</footer>
-
-</body>
-</html>
+<?php include('includes/footer.php'); ?>
