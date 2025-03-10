9aeb3df6d70ae42e1195ab18943de58415fcd920
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-10 09:51:46 +0530

    Add About Us page with header and footer includes

---
+++ about-us.php
@@ -0,0 +1,14 @@
+<?php include('includes/header.php'); ?>
+
+<main>
+    <article>
+        <h1>About Us</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum odit molestiae vel impedit expedita voluptatibus placeat adipisci perspiciatis enim, maiores, quas iure rem eligendi commodi sequi? Iure reiciendis aut repudiandae.</p>
+
+    </article>
+
+</main>
+
+
+<?php include('includes/footer.php'); ?>
